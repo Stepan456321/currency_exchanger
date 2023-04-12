b3045d6857f0e40f48e2bff9be110aafb4c5b899
Stepan456321 <130591165+Stepan456321@users.noreply.github.com>
2023-04-12 20:47:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,41 @@
 import java.util.Scanner;
 
-public class CurrencyConverter {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		double USD = 1.0;
-		double EUR = 1.1;
-		double GBP = 1.25;
-		
-		System.out.printin("Выберите валюту для конвертации (USD, EUR, GBP):");
-		String currency = scanner.next();
-		
-		System.out.printin("Введите количество валюты:");
-		String amount = scanner.next();
-		
-		double result = 0.0;
-		
-		switch (currency) {
-			case "USD":
-				result = amount * USD;
-				break;
-			case "EUR":
-				result = amount * EUR;
-				break;
-			case "GBP":
-				result = amount * GBP;
-				break;
-			default:
-				System.out.printin("Неверный выбор валюты!");
-					return;
-			}
-			
-			System.out.printin("Результат конвертации:" + result + " " + currency);
-			  }
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double rateUSD, rateEUR, rateGBP;
+
+        double rateUSD = 1;
+
+        System.out.println("Введите курс евро к доллару:");
+        rateEUR = scanner.nextDouble();
+
+        System.out.println("Введите курс фунта стерлингов к доллару:");
+        rateGBP = scanner.nextDouble();
+
+        System.out.println("Введите сумму для конвертации:");
+        double amount = scanner.nextDouble();
+
+        System.out.println("Выберите валюту для конвертации (USD, EUR, GBP):");
+        String currency = scanner.next();
+
+        double result = 0.0;
+
+        switch (currency) {
+            case "USD":
+                result = amount * rateUSD;
+                break;
+            case "EUR":
+                result = amount * rateEUR;
+                break;
+            case "GBP":
+                result = amount * rateGBP;
+                break;
+            default:
+                System.out.println("Неверный выбор валюты.");
+                return;
+        }
+
+        System.out.println("Результат конвертации: " + result + " " + currency);
+    }
 }
-				
